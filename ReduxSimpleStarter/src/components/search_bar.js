60324e05ac5e80c7da09f56281109978e4e37ba1
Ryan Silowash <resilowash@gmail.com>
2017-12-27 06:36:46 -0500

    working check in created searchbar component

---
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//import react any time you are using JSX.
+
+const SearchBar = () => {
+  return <input />;
+};
+
+export default SearchBar;  
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,4 +1,25 @@
+// get react from installed modules and allow access to it in this file
+import React from 'react';
+//need to import react-dom in order to render to the DOM
+import ReactDOM from 'react-dom';
+
+//local imports relative path to the file we are importing from
+import SearchBar from './components/search_bar';
+
+// I don't like storing this here... Security risk..  API key from google for youtube.
+const API_KEY='AIzaSyDrnzQFNaoNVj9pZnmhY7XL-7PranyuNos';
+
 // Create a new component
 // Component should produce some HTML
 
+const App = function() {
+  return (<div>
+    <SearchBar />
+  </div>);  
+}
+
+
+
 // Component needs inserted into the DOM. Component's generated html is placed on the page (via the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
