5434669d0bf1533848489d9f4b9a2b757b511614
Ryan Silowash <rsilowash@iq-inc.com>
2017-12-29 14:43:20 -0500

    Working on event handlers

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 //import react any time you are using JSX.
 
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends Component {
+  //render must be implemented when you extend React.Component
+  render() {
+    return <input onChange={(event) => console.log(event.target.value)} />;
+  }
 
-export default SearchBar;  
+  //event syntax start with 'on' or 'handle' <element><actionperformed>
+  //event handlers are called with event objects.
+  onInputChange(event) {
+    //on input change console log the changing input
+    console.log(event.target.value);
+  }
+
+}
+
+export default SearchBar;
