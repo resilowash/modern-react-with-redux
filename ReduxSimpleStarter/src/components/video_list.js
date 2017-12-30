8fe215b210e7e19e43170888d1edf199e9917cf5
Ryan Silowash <resilowash@gmail.com>
2017-12-29 22:43:25 -0500

    added key item to video_list_item as a property using youtube api etag

--- ReduxSimpleStarter/src/components/video_list.js
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -2,8 +2,10 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+  //looping through and array of videos via the array map method.
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video}/>
+    //define a key for the item based on the api, youtube search api has an etag for each video using key as property
+    return <VideoListItem key={video.etag} video={video}/>
   });
   return (
     <ul className="col-md-4 list-group">
