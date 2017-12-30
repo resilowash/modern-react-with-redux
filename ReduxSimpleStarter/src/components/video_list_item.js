345d11dd4034751ca14128b9bdfaac9acecbe92b
Ryan Silowash <resilowash@gmail.com>
2017-12-29 23:03:23 -0500

    Wired up components to start displaying thumbnail list of videos

--- ReduxSimpleStarter/src/components/video_list_item.js
+++ ReduxSimpleStarter/src/components/video_list_item.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+ // the {video} syntax is identical to const video = props.video. The first argume in the objects array has a property video, grab it an declare it calling it video
+  //in the video_list.js we created a property for video in the tag for VideoListItem we're pulling it out of the properties here.
+  //const video = props.video;
 
-  //in the video_list.js we created a property for video in the tag for VideoListItem we're pulling it out of the properties here. 
-  const video = props.video;
-  return <li>Video</li>
-}
+//className is bootstrap
+  const imageUrl = video.snippet.thumbnails.default.url;
+  return(
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default VideoListItem;
