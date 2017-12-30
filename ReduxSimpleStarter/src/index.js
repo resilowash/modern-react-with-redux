a05b415956d710476a0821d62aa4a5f0daad7883
Ryan Silowash <resilowash@gmail.com>
2017-12-29 22:25:22 -0500

    working check in building components and passing data to the videolist

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,28 +1,45 @@
 // get react from installed modules and allow access to it in this file
-import React from 'react';
+import React, { Component } from 'react';
 //need to import react-dom in order to render to the DOM
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 //local imports relative path to the file we are importing from
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 // I don't like storing this here... Security risk..  API key from google for youtube.
 const API_KEY='AIzaSyDrnzQFNaoNVj9pZnmhY7XL-7PranyuNos';
 
 //Sample search
-YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {console.log(data)}); 
+
 
 // Create a new component
 // Component should produce some HTML
-const App = function() {
-  return (<div>
-    <SearchBar />
-  </div>);
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      //with ES6 the syntax this.setState({videos: videos}) can be simply this.setState({ videos }). Syntactic sugar for key value pair that is the same.
+    });
+  }
+
+  render() {
+    return (<div>
+      <SearchBar />
+      <VideoList videos={this.state.videos}/>
+    </div>);
+  }
+
 }
 
 
 
 // Component needs inserted into the DOM. Component's generated html is placed on the page (via the DOM)
-
+//html entry point is the div container
 ReactDOM.render(<App />, document.querySelector('.container'));
