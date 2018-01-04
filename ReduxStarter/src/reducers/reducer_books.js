61663b30f8f4aab79d7d36da07145ae350a538a5
Ryan Silowash <resilowash@gmail.com>
2018-01-04 07:09:59 -0500

    started working on section 4 redux tutorial

---
+++ ReduxStarter/src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+//it can be helpful to prepend reducer
+//create the reducer, then wire it up
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Harry Potter'},
+    {title: 'The Dark Tower'},
+    {title: 'Eloquent Ruby'}
+  ]
+}
