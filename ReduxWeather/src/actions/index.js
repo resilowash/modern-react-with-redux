85fddbde00f3a287e427deb7815b9e4f83cf5521
Ryan Silowash <rsilowash@iq-inc.com>
2018-02-01 17:12:39 -0500

    Working Check In Lecture 61

--- ReduxWeather/src/actions/index.js
+++ ReduxWeather/src/actions/index.js
@@ -6,12 +6,13 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+//Redux-Promise uses intercepts actions where the payload property is a promise and after the promise is resolved dispatches a new action with the same type and the unwrapped promise data.
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   //this returns a promise
   const request = axios.get(url);
 
-  console.log('REQUEST: ', request); 
+  console.log('REQUEST: ', request);
 
   return {
     type: FETCH_WEATHER,
